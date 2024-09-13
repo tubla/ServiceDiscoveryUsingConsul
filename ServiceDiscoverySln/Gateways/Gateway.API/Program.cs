635282131dfe58c380b91088483a887aa5d001ce
RAHUL26R <rahulroy.profile@gmail.com>
2024-09-13 22:01:24 +0530

    Updated Ocelot.json to access services via consul when running as a docker container service. As of now it is not working, need to look into the issue.

--- ServiceDiscoverySln/Gateways/Gateway.API/Program.cs
+++ ServiceDiscoverySln/Gateways/Gateway.API/Program.cs
@@ -8,10 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 // For running ocelot.Local.json set "ASPNETCORE_ENVIRONMENT": "Local" in launchSettings.json
-// For running ocelot.Development.json it will be read from the docker environment settings
+// For running ocelot.Development.json it will be read from the docker environment gateway.api settings
 // For running ocelot.ServiceDiscovery.json locally set "ASPNETCORE_ENVIRONMENT": "ServiceDiscoveryLocal" in launchSettings.json
+// For running ocelot.ServiceDiscovery.json locally set "ASPNETCORE_ENVIRONMENT": "ServiceDiscoveryDocker" in ironment gateway.api settings
 
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+
 builder.Services.AddOcelot()
                 .AddCacheManager(settings => settings.WithDictionaryHandle())
                 .AddConsul(); // Adding consul service discovey to ocelot gateway 
